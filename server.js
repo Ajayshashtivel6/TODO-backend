635428e7fe936dc0f747fab787042b5da776330d
Ajay Shashtivel <ajayshashtivel@gmail.com>
2025-12-19 10:13:43 +0530

    deployment

--- server.js
+++ server.js
@@ -9,8 +9,16 @@ const taskRoutes = require('./routes/tasks');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://todo-front-fawn.vercel.app', 'http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.json({ message: 'TaskFlow Backend API is running!' });
+});
 
 // Routes
 app.use('/api/auth', authRoutes);
